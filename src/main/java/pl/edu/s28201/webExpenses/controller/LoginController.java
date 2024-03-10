5f948112ca0a230ab4124ad50fd563eff0ea818b
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-10 18:45:03 +0100

    Removed redundant post and autowired elements

--- src/main/java/pl/edu/s28201/webExpenses/controller/LoginController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
 import pl.edu.s28201.webExpenses.model.AppUser;
 import pl.edu.s28201.webExpenses.repository.AppUserRepository;
-import pl.edu.s28201.webExpenses.service.PasswordEncoderService;
+//import pl.edu.s28201.webExpenses.service.PasswordEncoderService;
 
 import java.util.Optional;
 
@@ -21,13 +21,8 @@ import java.util.Optional;
 @Slf4j
 public class LoginController {
 
-    private PasswordEncoderService passwordService;
-    private AppUserRepository appUserRepository;
-
     @Autowired
-    public LoginController(PasswordEncoderService passwordService, AppUserRepository appUserRepository) {
-        this.passwordService = passwordService;
-        this.appUserRepository = appUserRepository;
+    public LoginController() {
     }
 
     @GetMapping
@@ -36,30 +31,4 @@ public class LoginController {
         return "login2";
     }
 
-    @PostMapping
-    public String redirectToExpensesPage(@Valid @ModelAttribute AppUser appUser, Errors errors, SessionStatus sessionStatus) {
-        log.info("POST: Inside redirectToExpensesPage()");
-        String email = appUser.getEmail();
-        String password = appUser.getPassword();
-        Optional<AppUser> userOptional = appUserRepository.findByEmail(email);
-
-        if (userOptional.isEmpty()) {
-            log.info("AppUser with email {} was not found.", email);
-            return "redirect:/login?error";
-        }
-        AppUser user = userOptional.get();
-        String pswEncoded = user.getPassword();
-
-        if (passwordService.validate(password, pswEncoded)) {
-            log.info("AppUser email {} successfully logged in", email);
-            return "redirect:/expenses";
-        }
-        log.info("Incorrect password for AppUser with email {}.", email);
-        return "redirect:/login?error";
-    }
-
-    @ModelAttribute(name = "AppUser")
-    public AppUser appUser() {
-        return new AppUser();
-    }
 }
