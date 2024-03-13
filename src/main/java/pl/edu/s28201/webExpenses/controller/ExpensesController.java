3498cc7b7853ea6870b8f3e188903e36e5ea2a2f
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-13 16:30:09 +0100

    Linked expenses.html with Expense model.

--- src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
@@ -3,22 +3,27 @@ package pl.edu.s28201.webExpenses.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.s28201.webExpenses.repository.ExpenseRepository;
 
 @Controller
 @RequestMapping("/expenses")
 @Slf4j
 public class ExpensesController {
 
-    @Autowired
-    public ExpensesController() {
+    private ExpenseRepository expenseRepository;
 
+    @Autowired
+    public ExpensesController(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
     }
 
     @GetMapping
-    public String displayHelloPage() {
+    public String displayHelloPage(Model model) {
         log.info("GET: Inside displayHelloPage()");
+        model.addAttribute("expenses", expenseRepository.findAll());
         return "expenses";
     }
 }
