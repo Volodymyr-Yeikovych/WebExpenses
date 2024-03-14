9084c4e6f5573f5891d29618e432faef8612d022
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-14 19:55:33 +0100

    Refactored display method, so that only expenses belonging to current user are displayed.

--- src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
@@ -2,10 +2,14 @@ package pl.edu.s28201.webExpenses.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.s28201.webExpenses.model.AppUser;
+import pl.edu.s28201.webExpenses.model.SecurityAppUser;
 import pl.edu.s28201.webExpenses.repository.ExpenseRepository;
 
 @Controller
@@ -13,7 +17,7 @@ import pl.edu.s28201.webExpenses.repository.ExpenseRepository;
 @Slf4j
 public class ExpensesController {
 
-    private ExpenseRepository expenseRepository;
+    private final ExpenseRepository expenseRepository;
 
     @Autowired
     public ExpensesController(ExpenseRepository expenseRepository) {
@@ -21,9 +25,17 @@ public class ExpensesController {
     }
 
     @GetMapping
-    public String displayHelloPage(Model model) {
-        log.info("GET: Inside displayHelloPage()");
-        model.addAttribute("expenses", expenseRepository.findAll());
+    public String displayExpensePage(Model model) {
+        log.info("GET: Inside displayExpensePage()");
+        AppUser currentUser = getUserFromSecurity();
+        model.addAttribute("expenses", expenseRepository.findExpensesByUser(currentUser));
         return "expenses";
+    }
+
+    private AppUser getUserFromSecurity() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        SecurityAppUser securityAppUser = (SecurityAppUser) authentication.getPrincipal();
+        return securityAppUser.getUser();
     }
 }
