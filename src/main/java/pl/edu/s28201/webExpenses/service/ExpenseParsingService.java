115e17e0cbc7cd319ec74cbb468e08ce1e8bcec4
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-14 19:54:18 +0100

    Added money format for expense.

--- src/main/java/pl/edu/s28201/webExpenses/service/ExpenseParsingService.java
+++ src/main/java/pl/edu/s28201/webExpenses/service/ExpenseParsingService.java
@@ -2,10 +2,13 @@ package pl.edu.s28201.webExpenses.service;
 
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.time.format.TextStyle;
+import java.util.Currency;
 import java.util.Locale;
 
 @Service
@@ -13,5 +16,12 @@ public class ExpenseParsingService {
 
     public static String formatExpenseDate(LocalDateTime localDateTime) {
         return localDateTime.getYear() + ": " + localDateTime.getMonth().getDisplayName(TextStyle.SHORT, Locale.UK) + " " + localDateTime.getDayOfMonth();
+    }
+
+    public static String formatMoneySpent(BigDecimal amount, Currency currency) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator(',');
+        symbols.setDecimalSeparator('.');
+        return new DecimalFormat("#,###.##", symbols).format(amount) + " " + currency.getSymbol();
     }
 }
