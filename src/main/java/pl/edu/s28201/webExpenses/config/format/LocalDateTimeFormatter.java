fdc2c57ff4e7138df3f6de862d93146b9008fc41
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-17 16:57:06 +0100

    Added LocalTime formatter for newExpenses thymeleaf html parsing.

---
+++ src/main/java/pl/edu/s28201/webExpenses/config/format/LocalDateTimeFormatter.java
@@ -0,0 +1,27 @@
+package pl.edu.s28201.webExpenses.config.format;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
+import org.springframework.lang.NonNull;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Locale;
+
+@Configuration
+public class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
+    @Override
+    @NonNull
+    public LocalDateTime parse(@NonNull String text, @NonNull Locale locale) throws ParseException {
+        LocalDate parsedTimeMade = LocalDate.parse(text);
+        return parsedTimeMade.atTime(LocalTime.now());
+    }
+
+    @Override
+    @NonNull
+    public String print(@NonNull LocalDateTime object, @NonNull Locale locale) {
+        return object.toString();
+    }
+}
