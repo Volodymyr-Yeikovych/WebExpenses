cea17a32d06f12acb6fb9282b16a518a251308ce
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-17 10:20:54 +0100

    Changed behaviour of some classes.

--- src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseRepository.java
+++ src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseRepository.java
@@ -1,6 +1,8 @@
 package pl.edu.s28201.webExpenses.repository;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import pl.edu.s28201.webExpenses.model.AppUser;
 import pl.edu.s28201.webExpenses.model.Expense;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Repository
 public interface ExpenseRepository extends CrudRepository<Expense, UUID> {
 
+    @NonNull
     List<Expense> findAll();
 
     List<Expense> findExpensesByUser(AppUser appUser);
