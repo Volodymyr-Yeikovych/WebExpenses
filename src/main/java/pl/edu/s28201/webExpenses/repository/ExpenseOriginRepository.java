9c7fddaf38bc71e8927875fcf0c80f98659ceb05
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-17 17:01:10 +0100

    Added 2 methods to find by name and by user.

--- src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseOriginRepository.java
+++ src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseOriginRepository.java
@@ -2,9 +2,17 @@ package pl.edu.s28201.webExpenses.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.edu.s28201.webExpenses.model.AppUser;
+import pl.edu.s28201.webExpenses.model.ExpenseCategory;
 import pl.edu.s28201.webExpenses.model.ExpenseOrigin;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ExpenseOriginRepository extends CrudRepository<ExpenseOrigin, UUID> {
+
+    Optional<ExpenseOrigin> findByName(String name);
+
+    List<ExpenseOrigin> findByUser(AppUser user);
 }
