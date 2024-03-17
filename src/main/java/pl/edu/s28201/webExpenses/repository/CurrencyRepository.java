bb749f0f8e9161d0ac60a693f1d35b119ad4fece
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-17 17:03:18 +0100

    Added currencies repo.

---
+++ src/main/java/pl/edu/s28201/webExpenses/repository/CurrencyRepository.java
@@ -0,0 +1,12 @@
+package pl.edu.s28201.webExpenses.repository;
+
+import java.util.Currency;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface CurrencyRepository {
+
+    Set<Currency> findAll();
+    Map<String, String> findAllCurrenciesCodeToNameMap();
+}
