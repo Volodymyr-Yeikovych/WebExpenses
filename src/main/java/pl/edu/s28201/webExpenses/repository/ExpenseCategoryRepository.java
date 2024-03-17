3d6ebadfe1678a44d21a162e1380282c631dfe86
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-17 17:02:07 +0100

    Added methods to find by name and by user.

--- src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseCategoryRepository.java
+++ src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseCategoryRepository.java
@@ -2,9 +2,16 @@ package pl.edu.s28201.webExpenses.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.edu.s28201.webExpenses.model.AppUser;
 import pl.edu.s28201.webExpenses.model.ExpenseCategory;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ExpenseCategoryRepository extends CrudRepository<ExpenseCategory, UUID> {
+
+    Optional<ExpenseCategory> findByName(String name);
+
+    List<ExpenseCategory> findByUser(AppUser user);
 }
