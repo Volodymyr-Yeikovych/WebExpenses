15471d6dfff83f4165ce00831d0346e31a4ded64
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-21 08:32:07 +0100

    Removed unused imports.

--- src/main/java/pl/edu/s28201/webExpenses/controller/LoginController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/LoginController.java
@@ -1,29 +1,15 @@
 package pl.edu.s28201.webExpenses.controller;
 
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.support.SessionStatus;
-import pl.edu.s28201.webExpenses.model.AppUser;
-import pl.edu.s28201.webExpenses.repository.AppUserRepository;
-//import pl.edu.s28201.webExpenses.service.PasswordEncoderService;
 
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/login")
 @Slf4j
 public class LoginController {
-
-    @Autowired
-    public LoginController() {
-    }
 
     @GetMapping
     public String displayLoginPage() {
