b6352c3cbc8df6184333d2f93d2312c2fb37c03a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-24 19:13:08 +0100

    Moved creation of new Expense to separate Controller. Added logic to sort expenses by different parameters.

--- src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/ExpensesController.java
@@ -1,21 +1,21 @@
 package pl.edu.s28201.webExpenses.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.s28201.webExpenses.model.*;
-import pl.edu.s28201.webExpenses.repository.CurrencyRepository;
-import pl.edu.s28201.webExpenses.repository.ExpenseCategoryRepository;
-import pl.edu.s28201.webExpenses.repository.ExpenseShopRepository;
+import pl.edu.s28201.webExpenses.model.expense.Expense;
+import pl.edu.s28201.webExpenses.model.expense.ExpenseSortType;
 import pl.edu.s28201.webExpenses.repository.ExpenseRepository;
+import pl.edu.s28201.webExpenses.service.ExpenseParsingService;
+import pl.edu.s28201.webExpenses.service.ExpenseSortingService;
 import pl.edu.s28201.webExpenses.service.SecurityService;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Currency;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping("/expenses")
@@ -23,79 +23,68 @@ import java.util.Optional;
 public class ExpensesController {
 
     private final ExpenseRepository expenseRepository;
-    private final ExpenseCategoryRepository categoryRepository;
-    private final ExpenseShopRepository shopRepository;
-    private final CurrencyRepository currencyRepository;
-
     private final SecurityService securityService;
+    private final ExpenseSortingService expenseSortingService;
 
     @Autowired
-    public ExpensesController(ExpenseRepository expenseRepository, ExpenseCategoryRepository categoryRepository, ExpenseShopRepository shopRepository, CurrencyRepository currencyRepository, SecurityService securityService) {
+    public ExpensesController(ExpenseRepository expenseRepository,
+                              SecurityService securityService, ExpenseSortingService expenseSortingService) {
         this.expenseRepository = expenseRepository;
-        this.categoryRepository = categoryRepository;
-        this.shopRepository = shopRepository;
-        this.currencyRepository = currencyRepository;
         this.securityService = securityService;
+        this.expenseSortingService = expenseSortingService;
     }
 
     @GetMapping
-    public String displayExpensePage(Model model) {
+    public String displayExpensePage(Model model, HttpSession session) {
         log.info("GET: Inside displayExpensePage()");
-        AppUser currentUser = securityService.getUserFromSecurity();
-//        model.addAttribute("expenses", expenseRepository.findExpensesByUser(currentUser));
-        ExpenseDto dto = new ExpenseDto();
-        dto.addAll(expenseRepository.findExpensesByUser(currentUser));
+        Object objectType = session.getAttribute("sortType");
+        String sortType = "date_desc";
+        if (objectType != null) {
+            sortType = (String) objectType;
+        }
+        model.addAttribute("sortType", sortType);
 
-        model.addAttribute("expensesDto", dto);
+        List<Expense> expenses = sortAndRetrieveExpenses(sortType);
+        model.addAttribute("expenses", expenses);
+
         return "expenses";
     }
 
+    private List<Expense> sortAndRetrieveExpenses(String strSort) {
+        ExpenseSortType sortType = expenseSortingService.getSortTypeFromString(strSort);
+
+        return getSortedExpenses(sortType);
+    }
+
+    private List<Expense> getSortedExpenses(ExpenseSortType sortType) {
+        AppUser user = securityService.getUserFromSecurity();
+
+        return switch (sortType) {
+            case DATE_ASC -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.ASC, "timeMade"));
+            default -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.DESC, "timeMade"));
+
+            case CATEGORY_ABC_ASC -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.ASC, "category.name"));
+            case CATEGORY_ABC_DESC -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.DESC, "category.name"));
+
+            case SHOP_ABC_ASC -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.ASC, "shop.name"));
+            case SHOP_ABC_DESC -> expenseRepository.findExpensesByUser(user, Sort.by(Sort.Direction.DESC, "shop.name"));
+
+            case MONEY_TO_USD_ASC -> expenseSortingService.sortByMoneyAsc(expenseRepository.findExpensesByUser(user));
+            case MONEY_TO_USD_DESC -> expenseSortingService.sortByMoneyDesc(expenseRepository.findExpensesByUser(user));
+        };
+    }
+
     @PostMapping
-    public String returnExpensesPage(@RequestParam("selectedExpenses") String selectedExpensesIds) {
+    public String returnExpensesPage(@RequestParam(value = "selectedExpenses", defaultValue = "") String selectedExpensesIds) {
         log.info("POST: Inside returnExpensesPage()");
-        log.info(selectedExpensesIds);
+        if (selectedExpensesIds != null && !selectedExpensesIds.isEmpty()) {
+            log.info(selectedExpensesIds);
+            List<UUID> ids = ExpenseParsingService.parseExpenseIds(selectedExpensesIds, ",");
+            log.info("Parsed: " + ids.toString());
+            expenseRepository.deleteAllById(ids);
+        } else {
+            log.info("No expenses selected");
+        }
         return "redirect:/expenses";
-    }
-
-    @GetMapping("/new")
-    public String displayNewExpensePage(Model model) {
-        log.info("GET: Inside displayNewExpensePage()");
-        AppUser currentUser = securityService.getUserFromSecurity();
-        model.addAttribute("expense", new Expense());
-        model.addAttribute("categories", categoryRepository.findByUser(currentUser));
-        model.addAttribute("shops", shopRepository.findByUser(currentUser));
-        model.addAttribute("currencies", currencyRepository.findAllCurrenciesCodeToNameMap());
-        return "newExpense";
-    }
-
-    @PostMapping("/new")
-    public String returnReadyExpense(@ModelAttribute Expense expense,
-                                     @RequestParam("moneySpent") String moneySpent,
-                                     @RequestParam("currency") String currency,
-                                     @RequestParam("category") String categoryName,
-                                     @RequestParam("shop") String shopName,
-                                     @RequestParam(value = "description", required = false) String description) {
-        log.info("POST: Inside returnReadyExpense()");
-
-        Optional<ExpenseCategory> category = categoryRepository.findByName(categoryName);
-        Optional<ExpenseShop> shops =  shopRepository.findByName(shopName);
-        BigDecimal parsedMoneySpent = new BigDecimal(moneySpent);
-        Currency parsedCurrency = Currency.getInstance(currency);
-
-        expense.setUser(securityService.getUserFromSecurity());
-        expense.setMoneySpent(parsedMoneySpent);
-        expense.setCurrency(parsedCurrency);
-        category.ifPresent(expense::setCategory);
-        shops.ifPresent(expense::setShop);
-        expense.setDescription(description);
-
-        expenseRepository.save(expense);
-
-        return "redirect:/expenses";
-    }
-
-    @ModelAttribute("expense")
-    public Expense expense() {
-        return new Expense();
     }
 }
