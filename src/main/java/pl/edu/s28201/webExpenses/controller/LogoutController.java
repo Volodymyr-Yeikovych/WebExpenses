e59254c4de30482984eefde407025fd5034a45e9
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-24 19:04:29 +0100

    Added logout controller to handle user logout

---
+++ src/main/java/pl/edu/s28201/webExpenses/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package pl.edu.s28201.webExpenses.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/logout")
+@Slf4j
+public class LogoutController {
+
+    @GetMapping
+    public String displayLogoutPage() {
+        log.info("GET: Inside displayLogoutPage()");
+        return "logout";
+    }
+}
