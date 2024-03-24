56a35e5600b8d9419cd94473558f025d9aa59772
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-24 19:09:34 +0100

    Added enum to handle sorting type.

---
+++ src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseSortType.java
@@ -0,0 +1,12 @@
+package pl.edu.s28201.webExpenses.model.expense;
+
+public enum ExpenseSortType {
+    DATE_ASC,
+    DATE_DESC,
+    MONEY_TO_USD_ASC,
+    MONEY_TO_USD_DESC,
+    SHOP_ABC_ASC,
+    SHOP_ABC_DESC,
+    CATEGORY_ABC_ASC,
+    CATEGORY_ABC_DESC
+}
