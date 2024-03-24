22356b5a6067c01bd169f8ab7958dd0909e9245e
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-24 19:02:55 +0100

    Added Custom logout page security config.

--- src/main/java/pl/edu/s28201/webExpenses/config/security/WebSecurityConfig.java
+++ src/main/java/pl/edu/s28201/webExpenses/config/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package pl.edu.s28201.webExpenses.config;
+package pl.edu.s28201.webExpenses.config.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,10 +17,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final CustomLogoutHandler logoutHandler;
 
     @Autowired
-    public WebSecurityConfig(UserDetailsService userDetailsService) {
+    public WebSecurityConfig(UserDetailsService userDetailsService, CustomLogoutHandler logoutHandler) {
         this.userDetailsService = userDetailsService;
+        this.logoutHandler = logoutHandler;
     }
 
     @Bean
@@ -32,7 +33,12 @@ public class WebSecurityConfig {
                         auth.requestMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/webjars/**").permitAll()
                                 .requestMatchers("/", "/landing", "/login**", "/error/**", "/callback/").permitAll()
                                 .anyRequest().authenticated())
-                .logout(LogoutConfigurer::permitAll)
+                .logout(httpSecurityLogoutConfigurer ->
+                        httpSecurityLogoutConfigurer.logoutUrl("/logout")
+                                .addLogoutHandler(logoutHandler)
+//                                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll())
                 .formLogin(form -> form.loginPage("/login")
                         .loginProcessingUrl("/login")
                         .failureUrl("/login?error"));
