6927a837753b1632de03e29b2691290a2de17c7e
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-29 17:41:08 +0100

    Added dto to assist creation of new expense.

---
+++ src/main/java/pl/edu/s28201/webExpenses/model/dto/ExpenseDto.java
@@ -0,0 +1,20 @@
+package pl.edu.s28201.webExpenses.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ExpenseDto {
+
+    private UUID id;
+    private String timeMade;
+    private String moneyAndCurrency;
+    private String category;
+    private String shop;
+    private String description;
+}
