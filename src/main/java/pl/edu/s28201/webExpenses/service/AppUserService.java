ddcb97d0b69b986d8a56f4ec1e06c07c46a9d3bb
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-29 14:22:56 +0100

    Created AppUserService to handle logic related to AppUser

---
+++ src/main/java/pl/edu/s28201/webExpenses/service/AppUserService.java
@@ -0,0 +1,66 @@
+package pl.edu.s28201.webExpenses.service;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.edu.s28201.webExpenses.exception.AppUserAlreadyExistsException;
+import pl.edu.s28201.webExpenses.model.AppUser;
+import pl.edu.s28201.webExpenses.model.AppUserDto;
+import pl.edu.s28201.webExpenses.repository.AppUserRepository;
+
+@Service
+@Transactional
+@Slf4j
+public class AppUserService {
+
+    private final AppUserRepository repository;
+    private final PasswordEncoder encoder;
+    @Autowired
+    public AppUserService(AppUserRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    public AppUser registerNewUserAccount(AppUserDto dto) throws AppUserAlreadyExistsException {
+        if (emailExists(dto.getEmail())) {
+            throw new AppUserAlreadyExistsException("User with given email [" + dto.getEmail() + "] already exists!");
+        }
+
+        AppUser user = AppUser.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .email(dto.getEmail())
+                .password(encoder.encode(dto.getPassword()))
+                .build();
+
+        repository.save(user);
+
+        log.info("Got from Registration: " + user);
+
+        return user;
+    }
+
+    private boolean emailExists(String email) {
+        return repository.findByEmail(email).isPresent();
+    }
+
+    public String getUsernameFromUser(AppUserDto userDto) {
+        return getUsername(userDto.getFirstName(), userDto.getLastName());
+    }
+
+    public String getUsernameFromUser(AppUser user) {
+        return getUsername(user.getFirstName(), user.getLastName());
+    }
+
+    private String getUsername(String firstName, String lastName) {
+        StringBuilder username = new StringBuilder(firstName);
+
+        if (!lastName.isEmpty()) {
+            username.append(" ").append(lastName);
+        }
+
+        return username.toString();
+    }
+}
