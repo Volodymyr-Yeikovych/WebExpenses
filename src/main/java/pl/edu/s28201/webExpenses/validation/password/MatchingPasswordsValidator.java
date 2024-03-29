501b049c8626f86261b305e8a9bf2bbc0b9d38d8
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-29 14:21:20 +0100

    Created custom validator for comparing 2 passwords of AppUserDto class.

---
+++ src/main/java/pl/edu/s28201/webExpenses/validation/password/MatchingPasswordsValidator.java
@@ -0,0 +1,17 @@
+package pl.edu.s28201.webExpenses.validation.password;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import pl.edu.s28201.webExpenses.model.AppUserDto;
+
+public class MatchingPasswordsValidator implements ConstraintValidator<MatchingPasswords, Object> {
+
+    @Override
+    public void initialize(MatchingPasswords constraint) {}
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext context) {
+        AppUserDto user = (AppUserDto) o;
+        return user.getPassword().equals(user.getMatchingPassword());
+    }
+}
