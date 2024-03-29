79532820fe6a55730025786b36a4a38385f94713
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-29 14:21:32 +0100

    Created custom annotation for comparing 2 passwords of AppUserDto class.

---
+++ src/main/java/pl/edu/s28201/webExpenses/validation/password/MatchingPasswords.java
@@ -0,0 +1,23 @@
+package pl.edu.s28201.webExpenses.validation.password;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE,ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = MatchingPasswordsValidator.class)
+@Documented
+public @interface MatchingPasswords {
+
+    String message() default "Passwords do not match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
