170c3982e69db8e07b45e30709f43a313d5ccc88
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-30 18:36:24 +0100

    Added methods: find by name ignoring case and user, find all shops by user and is not hidden, hideAllById, hideById, showById

--- src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseShopRepository.java
+++ src/main/java/pl/edu/s28201/webExpenses/repository/ExpenseShopRepository.java
@@ -1,5 +1,8 @@
 package pl.edu.s28201.webExpenses.repository;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.edu.s28201.webExpenses.model.AppUser;
 import pl.edu.s28201.webExpenses.model.expense.ExpenseShop;
@@ -7,10 +10,33 @@ import pl.edu.s28201.webExpenses.model.expense.ExpenseShop;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public interface ExpenseShopRepository extends CrudRepository<ExpenseShop, UUID> {
 
     Optional<ExpenseShop> findByName(String name);
 
+    Optional<ExpenseShop> findByNameIgnoreCaseAndUser(String name, AppUser user);
+
     List<ExpenseShop> findByUser(AppUser user);
+
+    default List<ExpenseShop> findAllByUserAndNotHidden(AppUser user) {
+        return findByUser(user).stream().filter(shop -> !shop.isHidden()).collect(Collectors.toList());
+    }
+
+    @Modifying
+    @Transactional
+    default void hideAllById(List<UUID> ids) {
+        for (UUID id : ids) hideById(id);
+    }
+
+    @Modifying
+    @Query("UPDATE ExpenseShop s SET s.isHidden = true WHERE s.id = ?1")
+    @Transactional
+    void hideById(UUID id);
+
+    @Modifying
+    @Query("UPDATE ExpenseShop s SET s.isHidden = false WHERE s.id = ?1")
+    @Transactional
+    void showById(UUID id);
 }
