9d923e5bf86c62eae2b9230dfcd23a359a41954c
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-30 18:38:43 +0100

    Added logic to hide categories/shops and to display only not hidden cat/shops.

--- src/main/java/pl/edu/s28201/webExpenses/controller/DeleteCategoryShopController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/DeleteCategoryShopController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import pl.edu.s28201.webExpenses.repository.ExpenseCategoryRepository;
 import pl.edu.s28201.webExpenses.repository.ExpenseShopRepository;
 import pl.edu.s28201.webExpenses.service.SecurityService;
+import pl.edu.s28201.webExpenses.service.UuidService;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @Slf4j
@@ -19,29 +23,45 @@ public class DeleteCategoryShopController {
     private final ExpenseCategoryRepository categoryRepository;
     private final ExpenseShopRepository shopRepository;
     private final SecurityService securityService;
+    private final UuidService uuidService;
 
-    public DeleteCategoryShopController(ExpenseCategoryRepository categoryRepository, ExpenseShopRepository shopRepository, SecurityService securityService) {
+    public DeleteCategoryShopController(ExpenseCategoryRepository categoryRepository, ExpenseShopRepository shopRepository, SecurityService securityService, UuidService uuidService) {
         this.categoryRepository = categoryRepository;
         this.shopRepository = shopRepository;
         this.securityService = securityService;
+        this.uuidService = uuidService;
     }
 
     @GetMapping
     public String displayDeleteCategoryShopPage(Model model) {
         log.info("GET: Inside displayDeleteCategoryShopPage()");
 
-        model.addAttribute("categories", categoryRepository.findByUser(securityService.getUserFromSecurity()));
-        model.addAttribute("shops", shopRepository.findByUser(securityService.getUserFromSecurity()));
+        model.addAttribute("categories", categoryRepository.findAllByUserAndNotHidden(securityService.getUserFromSecurity()));
+        model.addAttribute("shops", shopRepository.findAllByUserAndNotHidden(securityService.getUserFromSecurity()));
 
         return "deleteShopCategory";
     }
 
     @PostMapping
-    public String returnExpensesPage(@RequestParam("selectedCategories") String categories, @RequestParam("selectedShops") String shops) {
+    public String returnExpensesPage(@RequestParam(value = "selectedCategories", defaultValue = "") String categories,
+                                     @RequestParam(value = "selectedShops", defaultValue = "") String shops) {
         log.info("POST: Inside returnExpensesPage()");
 
-        log.info("Categories to delete: [{}]", categories);
-        log.info("Shops to delete: [{}]", shops);
+        if (categories != null && !categories.isEmpty()) {
+            List<UUID> ids = uuidService.parseExpenseIds(categories, ",");
+            log.info("Parsed Category IDs to Hide: " + ids.toString());
+            categoryRepository.hideAllById(ids);
+        } else {
+            log.info("No categories to hide");
+        }
+
+        if (shops != null && !shops.isEmpty()) {
+            List<UUID> ids = uuidService.parseExpenseIds(shops, ",");
+            log.info("Parsed Shop IDs to Hide: " + ids.toString());
+            shopRepository.hideAllById(ids);
+        } else {
+            log.info("No shops to hide");
+        }
 
         return "redirect:/expenses";
     }
