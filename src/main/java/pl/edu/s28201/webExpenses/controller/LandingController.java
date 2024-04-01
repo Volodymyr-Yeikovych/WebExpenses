d0c2d7ea6cd70f41850964e6fafd92a926a09553
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-01 19:40:54 +0200

    Removed unnecessary imports.

--- src/main/java/pl/edu/s28201/webExpenses/controller/LandingController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/LandingController.java
@@ -1,7 +1,6 @@
 package pl.edu.s28201.webExpenses.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
