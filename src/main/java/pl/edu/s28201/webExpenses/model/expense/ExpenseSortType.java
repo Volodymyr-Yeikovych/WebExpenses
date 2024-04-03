69c7693ca301e7a34041f226b72169f47a980d05
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-03 13:53:50 +0200

    Added String to ExpenseSortType parsing method.

--- src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseSortType.java
+++ src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseSortType.java
@@ -1,12 +1,32 @@
 package pl.edu.s28201.webExpenses.model.expense;
 
 public enum ExpenseSortType {
-    DATE_ASC,
-    DATE_DESC,
-    MONEY_TO_USD_ASC,
-    MONEY_TO_USD_DESC,
-    SHOP_ABC_ASC,
-    SHOP_ABC_DESC,
-    CATEGORY_ABC_ASC,
-    CATEGORY_ABC_DESC
+    DATE_ASC("date_asc"),
+    DATE_DESC("date_desc"),
+    MONEY_TO_USD_ASC("money_to_usd_asc"),
+    MONEY_TO_USD_DESC("money_to_usd_desc"),
+    SHOP_ABC_ASC("shop_asc"),
+    SHOP_ABC_DESC("shop_desc"),
+    CATEGORY_ABC_ASC("category_asc"),
+    CATEGORY_ABC_DESC("category_desc");
+
+    private final String sortType;
+
+    ExpenseSortType(String sortType) {
+        this.sortType = sortType;
+    }
+
+    public static ExpenseSortType fromString(String strSort) {
+        return switch (strSort) {
+            case "date_asc" -> DATE_ASC;
+            case "date_desc" -> DATE_DESC;
+            case "money_to_usd_asc" -> MONEY_TO_USD_ASC;
+            case "money_to_usd_desc" -> MONEY_TO_USD_DESC;
+            case "category_asc" -> CATEGORY_ABC_ASC;
+            case "category_desc" -> CATEGORY_ABC_DESC;
+            case "shop_asc" -> SHOP_ABC_ASC;
+            case "shop_desc" -> SHOP_ABC_DESC;
+            default -> throw new IllegalArgumentException("Passed sort type is not supported: " + strSort);
+        };
+    }
 }
