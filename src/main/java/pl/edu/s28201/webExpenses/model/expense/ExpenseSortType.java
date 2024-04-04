402b9fce06ded4b93ac380c22e84e4a1341c9209
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-04 10:00:32 +0200

    Added caching and removed redundant switch.

--- src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseSortType.java
+++ src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseSortType.java
@@ -1,5 +1,8 @@
 package pl.edu.s28201.webExpenses.model.expense;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ExpenseSortType {
     DATE_ASC("date_asc"),
     DATE_DESC("date_desc"),
@@ -10,6 +13,14 @@ public enum ExpenseSortType {
     CATEGORY_ABC_ASC("category_asc"),
     CATEGORY_ABC_DESC("category_desc");
 
+    private static final Map<String, ExpenseSortType> BY_SORT_TYPE = new HashMap<>();
+
+    static {
+        for (ExpenseSortType type : values()) {
+            BY_SORT_TYPE.put(type.sortType, type);
+        }
+    }
+
     private final String sortType;
 
     ExpenseSortType(String sortType) {
@@ -17,16 +28,6 @@ public enum ExpenseSortType {
     }
 
     public static ExpenseSortType fromString(String strSort) {
-        return switch (strSort) {
-            case "date_asc" -> DATE_ASC;
-            case "date_desc" -> DATE_DESC;
-            case "money_to_usd_asc" -> MONEY_TO_USD_ASC;
-            case "money_to_usd_desc" -> MONEY_TO_USD_DESC;
-            case "category_asc" -> CATEGORY_ABC_ASC;
-            case "category_desc" -> CATEGORY_ABC_DESC;
-            case "shop_asc" -> SHOP_ABC_ASC;
-            case "shop_desc" -> SHOP_ABC_DESC;
-            default -> throw new IllegalArgumentException("Passed sort type is not supported: " + strSort);
-        };
+        return BY_SORT_TYPE.get(strSort);
     }
 }
