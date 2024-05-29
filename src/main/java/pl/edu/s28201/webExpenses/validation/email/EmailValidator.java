757a7297e946d8c48d8f68fa6d6f08837e643f20
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-29 13:42:06 +0200

    Minor fixes.

--- src/main/java/pl/edu/s28201/webExpenses/validation/email/EmailValidator.java
+++ src/main/java/pl/edu/s28201/webExpenses/validation/email/EmailValidator.java
@@ -8,10 +8,12 @@ import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<EmailValidation, String> {
 
+
     private Pattern pattern;
     private Matcher matcher;
     private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" +
             "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$";
+
     @Override
     public void initialize(EmailValidation annotation) {
     }
