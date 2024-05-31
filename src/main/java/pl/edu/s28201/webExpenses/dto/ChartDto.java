25d08bbfe36542e3673c1121d6a934809a9ac1af
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-31 20:02:23 +0200

    Added validation.

--- src/main/java/pl/edu/s28201/webExpenses/dto/ChartDto.java
+++ src/main/java/pl/edu/s28201/webExpenses/dto/ChartDto.java
@@ -1,15 +1,21 @@
 package pl.edu.s28201.webExpenses.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import pl.edu.s28201.webExpenses.model.BarChartParameter;
+import pl.edu.s28201.webExpenses.validation.time.Date;
 
 import java.time.LocalDate;
 
 @Data
 public class ChartDto {
-    // TODO: FROM - TO >= barChart!!!!
+    @Date
     private LocalDate from;
+    @Date
     private LocalDate to;
     private BarChartParameter parameter;
+    @Min(value = 3)
+    @Max(value = 12)
     private int barCount;
 }
