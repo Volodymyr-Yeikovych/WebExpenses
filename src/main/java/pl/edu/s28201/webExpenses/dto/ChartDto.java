3215523883d11893d99260525e26142e9a45b38c
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-31 14:15:53 +0200

    Minor fixes.

--- src/main/java/pl/edu/s28201/webExpenses/dto/ChartDto.java
+++ src/main/java/pl/edu/s28201/webExpenses/dto/ChartDto.java
@@ -1,15 +1,15 @@
 package pl.edu.s28201.webExpenses.dto;
 
 import lombok.Data;
-import pl.edu.s28201.webExpenses.model.ChartParameter;
+import pl.edu.s28201.webExpenses.model.BarChartParameter;
 
 import java.time.LocalDate;
 
 @Data
 public class ChartDto {
-    // TODO: FROM - TO >= 12!!!!
+    // TODO: FROM - TO >= barChart!!!!
     private LocalDate from;
     private LocalDate to;
-    private ChartParameter parameter;
+    private BarChartParameter parameter;
     private int barCount;
 }
