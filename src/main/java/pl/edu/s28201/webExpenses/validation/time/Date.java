13a330b27aa734069b3c1519e7a5cf7eefce15c7
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-31 20:03:31 +0200

    Added validation annotation for LocalDate.

---
+++ src/main/java/pl/edu/s28201/webExpenses/validation/time/Date.java
@@ -0,0 +1,21 @@
+package pl.edu.s28201.webExpenses.validation.time;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = LocalDateValidator.class)
+@Documented
+public @interface Date {
+    String message() default "Cannot set Date to null or value that is in the future.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
