92ad2b15b87b87f1b985b076efd5cbdc39edaf14
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-01 17:19:17 +0200

    Added error handling to post method.

--- src/main/java/pl/edu/s28201/webExpenses/controller/ChartGenerationController.java
+++ src/main/java/pl/edu/s28201/webExpenses/controller/ChartGenerationController.java
@@ -1,28 +1,33 @@
 package pl.edu.s28201.webExpenses.controller;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.edu.s28201.webExpenses.dto.ChartDto;
 import pl.edu.s28201.webExpenses.model.BarChartParameter;
-import pl.edu.s28201.webExpenses.service.ChartService;
+import pl.edu.s28201.webExpenses.service.chart.ChartService;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/chart")
+@Slf4j
 public class ChartGenerationController {
 
     private final ChartService chartService;
 
     @Autowired
-    public ChartGenerationController(ChartService chartService) {
+    public ChartGenerationController(
+            ChartService chartService
+    ) {
         this.chartService = chartService;
     }
 
@@ -31,7 +36,9 @@ public class ChartGenerationController {
             Model model,
             @ModelAttribute("data") List<BigDecimal> data,
             @ModelAttribute("labels") List<String> labels
-            ) {
+    ) {
+        log.info("GET: /chart/display");
+
         model.addAttribute("labels", labels);
         model.addAttribute("data", data);
 
@@ -39,15 +46,41 @@ public class ChartGenerationController {
     }
 
     @GetMapping
-    public String displayChartParametersSelection(Model model) {
+    public String displayChartParametersSelection(
+            Model model,
+            @ModelAttribute("invalidDateRange") String invalidDateRange,
+            @ModelAttribute("errors") ArrayList<String> errors
+    ) {
+        log.info("GET: /chart");
+
         model.addAttribute("dto", new ChartDto());
         model.addAttribute("chartParams", BarChartParameter.values());
+        model.addAttribute("invalidDateRange", invalidDateRange);
+        model.addAttribute("errors", errors);
 
         return "generateChart";
     }
 
     @PostMapping
-    public String returnChartParameters(Model model, @ModelAttribute ChartDto dto, RedirectAttributes ra) {
+    public String returnChartParameters(
+            @ModelAttribute @Valid ChartDto dto,
+            Errors errors,
+            RedirectAttributes ra
+    ) {
+        log.info("POST: /chart");
+        boolean invalidDate = !chartService.isValidDate(dto.getFrom(), dto.getTo(), dto.getBarCount());
+
+        if (invalidDate) {
+            ra.addFlashAttribute("invalidDateRange", "Days range should be more or equal to number of bar chart columns.");
+        }
+
+        if (errors.hasErrors()) {
+            ra.addFlashAttribute("errors", errors.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList());
+        }
+
+        if (invalidDate || errors.hasErrors()) {
+            return "redirect:/chart";
+        }
 
         var chartData = chartService.getChartDataFromParameters(dto);
 
