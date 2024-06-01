f8475abef94d181b84aab6a0609b533407b55196
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-01 17:14:23 +0200

    Added class to configure rest requests.

---
+++ src/main/java/pl/edu/s28201/webExpenses/config/RestConfig.java
@@ -0,0 +1,14 @@
+package pl.edu.s28201.webExpenses.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
