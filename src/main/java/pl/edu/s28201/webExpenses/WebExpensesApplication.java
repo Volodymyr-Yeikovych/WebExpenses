fb31ceb9897349739e9304baab5f3cce03e3bb27
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-01 17:11:34 +0200

    Minor fixes.

--- src/main/java/pl/edu/s28201/webExpenses/WebExpensesApplication.java
+++ src/main/java/pl/edu/s28201/webExpenses/WebExpensesApplication.java
@@ -1,14 +1,11 @@
 package pl.edu.s28201.webExpenses;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.edu.s28201.webExpenses.repository.*;
-import pl.edu.s28201.webExpenses.service.MockService;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Slf4j
