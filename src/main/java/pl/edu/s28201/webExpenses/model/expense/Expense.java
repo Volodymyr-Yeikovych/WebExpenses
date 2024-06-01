37f17de68867c868fd7ea48e75e0ccf5b53febb4
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-01 17:11:13 +0200

    Replaced @Data annotation.

--- src/main/java/pl/edu/s28201/webExpenses/model/expense/Expense.java
+++ src/main/java/pl/edu/s28201/webExpenses/model/expense/Expense.java
@@ -2,32 +2,28 @@ package pl.edu.s28201.webExpenses.model.expense;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import pl.edu.s28201.webExpenses.model.AppUser;
 import pl.edu.s28201.webExpenses.validation.time.DateTime;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
-@NoArgsConstructor
 @Builder
 @AllArgsConstructor
 public class Expense {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
     @ManyToOne(targetEntity = AppUser.class)
@@ -51,5 +47,21 @@ public class Expense {
         this.category = category;
         this.shop = shop;
         this.description = description;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Expense expense = (Expense) o;
+        return getId() != null && Objects.equals(getId(), expense.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
