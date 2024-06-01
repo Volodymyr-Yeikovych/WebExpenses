bd482d5e825d1eaccaa60e0c4bda692aa965c389
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-01 17:11:24 +0200

    Replaced @Data annotation.

--- src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseShop.java
+++ src/main/java/pl/edu/s28201/webExpenses/model/expense/ExpenseShop.java
@@ -1,17 +1,18 @@
 package pl.edu.s28201.webExpenses.model.expense;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import pl.edu.s28201.webExpenses.model.AppUser;
 
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ExpenseShop {
@@ -27,5 +28,21 @@ public class ExpenseShop {
     public ExpenseShop(String name, AppUser user) {
         this.user = user;
         this.name = name;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ExpenseShop that = (ExpenseShop) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
